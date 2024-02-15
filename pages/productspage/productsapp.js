adf0f6bc5432eb1583e0d1b761fdcdc571abd261
Nika Natenadze <151843072+nikanatenaze@users.noreply.github.com>
2024-02-15 18:54:49 +0400

    Reupload

--- pages/productspage/productsapp.js
+++ pages/productspage/productsapp.js
@@ -13,6 +13,7 @@ function getcards(e) {
         <p class="card-brand">By: ${element.brand}</p>
         <p class="card-gender">${element.gender}'s</p>
         <p class="card-sell">${element.sell}</p>
+        <p class="card-sell">price: ${element.price}$</p>
         <button class="card-more-btn">More</button>
     </div>`
     });
@@ -33,4 +34,53 @@ function stars(count) {
     return string
 }
 
-console.log(products);
+// სერტიროვკას ლოგიკა
+
+console.log(cardpg);
+
+let minpriceinput = document.getElementById("minprice")
+let maxpriceinput = document.getElementById("maxprice")
+let sertsearchbtn = document.getElementById("search-btn")
+
+let searchbyname = document.getElementById("search-by-name-input")
+
+
+sertsearchbtn.addEventListener("click", function(){
+    cardpg.innerHTML = ""
+    fetch("../../data/productsApi/products.json").then(info => info.json()).then(infoin => showprisedcards(infoin))
+    function showprisedcards(e) {
+        e.forEach(info => {
+            if(info.price <= maxpriceinput.value && info.price >= minpriceinput.value) {
+                cardpg.innerHTML += `<div class="product-card">
+                <img class="card-img" src="${info.image}" alt="">
+                <p class="card-title">${info.name}</p>
+                <p class="card-stars">${stars(info.stars)}</p>
+                <p class="card-brand">By: ${info.brand}</p>
+                <p class="card-gender">${info.gender}'s</p>
+                <p class="card-sell">${info.sell}</p>
+                <p class="card-sell">price: ${info.price}$</p>
+                <button class="card-more-btn">More</button>
+            </div>`
+            }
+        })
+        if(maxpriceinput.value === "" && minpriceinput.value === "") {
+            console.log(123);
+            e.forEach(info2 => {
+                cardpg.innerHTML += `<div class="product-card">
+                <img class="card-img" src="${info2.image}" alt="">
+                <p class="card-title">${info2.name}</p>
+                <p class="card-stars">${stars(info2.stars)}</p>
+                <p class="card-brand">By: ${info2.brand}</p>
+                <p class="card-gender">${info2.gender}'s</p>
+                <p class="card-sell">${info2.sell}</p>
+                <p class="card-sell">price: ${info2.price}$</p>
+                <button class="card-more-btn">More</button>
+            </div>`
+            })
+        }
+    }
+})
+
+// სახელით ძებნა
+
+
